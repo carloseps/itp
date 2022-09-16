1ba250d8e1bbf593eb3153ae35e695a3b04ca76a
Carlos Pinheiro <pinheirosilvacarlos@gmail.com>
2022-09-16 15:44:32 -0300

    Update 4.c from exam3

--- exam3/4.c
+++ exam3/4.c
@@ -1,41 +1,22 @@
 #include <stdio.h>
-#include <stdlib.h>
-void compararCoord(int retangulo1[], int retangulo2[]);
-void printaCoord(int vetor[]);
-void preencheVetor(int vetor[]);
-void inverteVetores(int vetor[], int vetor2[]);
+int terminou(float retangulo1[]);
 int main (void){
-  int retangulo1[4], retangulo2[4], auxiliar[4];
-  while (1){
-    preencheVetor(retangulo1);
-    if (retangulo1[0] == 0 && retangulo1[1] == 0 && retangulo1[2] == 0 && retangulo1[3] == 0){
-      inverteVetores(retangulo1, auxiliar);
-      break;
-    } else {
-      inverteVetores(auxiliar, retangulo1);
+  float retangulo1[4], retangulo2[4];
+  scanf("%f %f %f %f", &retangulo1[0], &retangulo1[1], &retangulo1[2], &retangulo1[3]);
+  while (terminou(retangulo2) == 0){
+    scanf("%f %f %f %f", &retangulo2[0], &retangulo2[1], &retangulo2[2], &retangulo2[3]);
+    if (terminou(retangulo2) == 0){
+      if(retangulo1[0] < retangulo2[0] && retangulo1[1] < retangulo2[1] && retangulo2[2] < retangulo1[2] && retangulo2[3] < retangulo1[3]){
+        printf("(<%.2f,%.2f>,<%.2f,%.2f>) contem (<%.2f,%.2f>,<%.2f,%.2f>).\n", retangulo1[0], retangulo1[1], retangulo1[2], retangulo1[3], retangulo2[0], retangulo2[1], retangulo2[2], retangulo2[3]);
+      } else if (retangulo1[0] > retangulo2[0] && retangulo1[1] > retangulo2[1] && retangulo2[2] > retangulo1[2] && retangulo2[3] > retangulo1[3]){
+        printf("(<%.2f,%.2f>,<%.2f,%.2f>) contem (<%.2f,%.2f>,<%.2f,%.2f>).\n", retangulo2[0], retangulo2[1], retangulo2[2], retangulo2[3], retangulo1[0], retangulo1[1], retangulo1[2], retangulo1[3]);
+      } else{
+        printf("Nao posso afirmar!\n"); 
+      }
     }
-    preencheVetor(retangulo2);
-    compararCoord(retangulo1, retangulo2);
   }
   return 0;
 }
-void compararCoord(int retangulo1[], int retangulo2[]){
-  int win1 = 0, win2 = 0;
-  for (int i = 0; i <= 3; i++){
-    if(retangulo1[i] < retangulo2[i]){
-      if(i >= 2){ win2++;} else{ win1++;}
-    } else if (retangulo1[i] > retangulo2[i]){
-      if(i >= 2){ win1++;} else{ win2++;}
-    }    
-  }
-  if(win1 == win2){
-    printf("Nao posso afirmar!");
-  } else if (win1 > win2){
-    printaCoord(retangulo1); printf(" contem "); printaCoord(retangulo2); printf(".");
-  } else {
-    printaCoord(retangulo2); printf(" contem "); printaCoord(retangulo1); printf(".");
-  }
+int terminou(float retangulo2[]){
+  if (retangulo2[0] == 0 && retangulo2[1] == 0 && retangulo2[2] == 0 && retangulo2[3] == 0) return 1; else return 0;
 }
-void printaCoord(int vetor[]){ printf("(<%d.00,%d.00>,<%d.00,%d.00>)", vetor[0], vetor[1], vetor[2], vetor[3]);}
-void preencheVetor(int vetor[]){for (int i = 0; i <= 3; i++){ scanf("%d", &vetor[i]);}}
-void inverteVetores(int vetor1[], int vetor2[]){for (int i = 0; i <= 3; i++){vetor1[i] = vetor2[i];}}
